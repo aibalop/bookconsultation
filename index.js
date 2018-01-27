064e38a05033de83b7b634777cb57bae1a588afe
Alan Ibarra <alan.ibarra@mxglobalsolutions.com>
2018-01-26 17:08:40 -0700

    Feat: adding request to the API and showing results

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const request = require('request');
+
 const app = express();
 
 app.set('view engine', 'pug');
@@ -12,6 +14,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(bodyParser.json());
 
 app.use(session({
     secret: "x675ax6a767sa6x7a6",
@@ -29,14 +32,90 @@ function auth(req, res, next) {
 
 app.get("/login", (req, res) => {
     res.status(200).render('login', {
-        title: "OpenBooks | Iniciar Sesión"
+        title: "OpenBooks | Log In"
     });
 });
 
 app.get('/', (req, res) => {
-    res.status(200).render('index', {
-        title: "OpenBooks | Your guid´s consulting",
-        usuario: req.session.user
+    request('http://www.etnassoft.com/api/v1/get/?get_categories=all',(errorCategories,responseCategories,bodyCategories)=>{
+        if (errorCategories) {
+            console.log(errorCategories);
+        }
+
+        var result_categories = bodyCategories.substring(1,bodyCategories.length-2);
+
+        request('http://www.etnassoft.com/api/v1/get/?book=all&criteria=most_viewed',(errorBooks,responseBooks,bodyBooks)=>{
+            if (errorBooks) {
+                console.log(errorBooks);
+            }            
+            
+            var result_books = bodyBooks.substring(1,bodyBooks.length-2);
+
+            res.status(200).render('index', {
+                title: "OpenBooks | Your guid´s consulting",
+                usuario: req.session.user,
+                categorias : JSON.parse(result_categories),
+                libros : JSON.parse(result_books)
+            });
+        });
+        
+    });
+});
+
+app.get('/categoria/:categoryid', (req, res) => {
+    request('http://www.etnassoft.com/api/v1/get/?get_categories=all',(errorCategories,responseCategories,bodyCategories)=>{
+        if (errorCategories) {
+            console.log(errorCategories);
+        }
+
+        var result_categories = bodyCategories.substring(1,bodyCategories.length-2);
+
+        let id = req.params.categoryid;
+        
+        request('http://www.etnassoft.com/api/v1/get/?book=all&criteria=most_viewed&category_id=' + id,(errorBooks,responseBooks,bodyBooks)=>{
+            if (errorBooks) {
+                console.log(errorBooks);
+            }            
+            
+            var result_books = bodyBooks.substring(1,bodyBooks.length-2);
+            
+            res.status(200).render('categoria', {
+                title: "OpenBooks | Your guid´s consulting",
+                usuario: req.session.user,
+                categorias : JSON.parse(result_categories),
+                libros : JSON.parse(result_books)
+            });
+        });
+        
+    });
+});
+
+app.post('/buscar', (req, res) => {
+    console.log(req.body.txtBook);
+    request('http://www.etnassoft.com/api/v1/get/?get_categories=all',(errorCategories,responseCategories,bodyCategories)=>{
+        if (errorCategories) {
+            console.log(errorCategories);
+        }
+
+        var result_categories = bodyCategories.substring(1,bodyCategories.length-2);
+
+        let pattern = req.body.txtBook;
+        console.log(pattern);
+        request('http://www.etnassoft.com/api/v1/get/?keyword=' + pattern,(errorBooks,responseBooks,bodyBooks)=>{
+            if (errorBooks) {
+                console.log(errorBooks);
+            }            
+            
+            var result_books = bodyBooks.substring(1,bodyBooks.length-2);
+            
+            res.status(200).render('categoria', {
+                title: "OpenBooks | Your guid´s consulting",
+                usuario: req.session.user,
+                categorias : JSON.parse(result_categories),
+                libros : JSON.parse(result_books)
+            });
+        });
+        
     });
 });
 
@@ -54,7 +133,7 @@ app.post('/login_process', (req, res) => {
         res.status(200).redirect('/');
     } else {
         res.status(200).render('login', {
-        	title : "OpenBooks | Iniciar Sesión",
+        	title : "OpenBooks | Log In",
             mensaje: "Datos incorrectos"
         });
     }
